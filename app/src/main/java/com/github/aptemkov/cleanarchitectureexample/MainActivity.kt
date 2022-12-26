b1629b649d12e1ae225407a036434e41741377fe
aptemkov <artkov37@gmail.com>
2022-12-26 18:21:31 +0300

    Init commit 2

--- app/src/main/java/com/github/aptemkov/cleanarchitectureexample/MainActivity.kt
+++ app/src/main/java/com/github/aptemkov/cleanarchitectureexample/MainActivity.kt
@@ -2,10 +2,27 @@ package com.github.aptemkov.cleanarchitectureexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataTextView = findViewById<TextView>(R.id.dataTextView)
+        val dataEditView = findViewById<EditText>(R.id.dataEditText)
+        val sendButton = findViewById<Button>(R.id.sendButton)
+        val receiveButton = findViewById<Button>(R.id.receiveButton)
+
+        sendButton.setOnClickListener {
+
+        }
+
+        receiveButton.setOnClickListener {
+
+        }
+
     }
 }
